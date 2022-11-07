2fb1038f90f0d43903cfe4beea6e81c7548ddb70
Vinayak54 <98582574+Vinayak54@users.noreply.github.com>
2022-11-07 14:58:31 +0530

    Create fibonnaci.py

---
+++ fibonnaci.py
@@ -0,0 +1,14 @@
+
+number = 10
+print("Fibonacci series of 10 numbers is")
+
+n1, n2 = 0, 1
+count = 0
+
+while count < number:
+    print(n1)
+    a = n1 + n2
+
+    n1 = n2
+    n2 = a
+    count += 1
